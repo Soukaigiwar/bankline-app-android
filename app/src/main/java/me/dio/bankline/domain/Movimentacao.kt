02eb7637f4c158458bbb7ee2c8025332cdb04149
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2022-05-09 00:41:44 -0300

    initial commit

---
+++ app/src/main/java/me/dio/bankline/domain/Movimentacao.kt
@@ -0,0 +1,11 @@
+package me.dio.bankline.domain
+
+data class Movimentacao(
+    val id: Int,
+    val dataHora: String,
+    val descricao: String,
+    val valor: Double,
+    val tipo: TipoMovimentacao,
+    // TODO mapear idConta como idCorrentista
+    val idCorrentista: Int
+)
