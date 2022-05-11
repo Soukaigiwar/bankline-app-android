0bd4361abbfe0935db44d83df4a08dec1d12d90b
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2022-05-11 01:58:39 -0300

    finished dev week

--- app/src/main/java/me/dio/bankline/ui/statement/BankStatementActivity.kt
+++ app/src/main/java/me/dio/bankline/ui/statement/BankStatementActivity.kt
@@ -3,10 +3,16 @@ package me.dio.bankline.ui.statement
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import me.dio.bankline.R
+import me.dio.bankline.data.State
 import me.dio.bankline.databinding.ActivityBankStatementBinding
 import me.dio.bankline.databinding.ActivityWelcomeBinding
 import me.dio.bankline.domain.Correntista
+import me.dio.bankline.domain.Movimentacao
+import me.dio.bankline.domain.TipoMovimentacao
 import java.lang.IllegalArgumentException
 
 class BankStatementActivity : AppCompatActivity() {
@@ -23,10 +29,32 @@ class BankStatementActivity : AppCompatActivity() {
         intent.getParcelableExtra<Correntista>(EXTRA_ACCOUNT_HOLDER) ?: throw IllegalArgumentException()
     }
 
+    private val viewModel by viewModels<BankStatementViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        Log.d("teste", "chegou o id: ${accountHolder}")
+        binding.rvBankStatement.layoutManager = LinearLayoutManager(this)
+
+        findBankStatement()
+
+        binding.srlBankStatement.setOnRefreshListener { findBankStatement() }
+    }
+
+    private fun findBankStatement() {
+        viewModel.findBankStatement(accountHolder.id).observe(this){ state ->
+            when(state) {
+                is State.Success -> {
+                    binding.rvBankStatement.adapter = state.data?.let { BankStatementAdapter(it) }
+                    binding.srlBankStatement.isRefreshing = false
+                }
+                is State.Error -> {
+                    state.message?.let { Snackbar.make(binding.rvBankStatement, it, Snackbar.LENGTH_LONG).show() }
+                    binding.srlBankStatement.isRefreshing = false
+                }
+                State.Wait -> binding.srlBankStatement.isRefreshing = true
+            }
+        }
     }
 }
